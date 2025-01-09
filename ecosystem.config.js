c9028d13375802d0638aefab1a85256aba55e40d
niloyahmedrasel <niloyahmedrasel24@gmail.com>
2025-01-09 15:51:14 +0600

    updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "identity", // Application name
-      script: "index.js", // Entry point of your app (adjust if it's not `index.js`)
+      script: "dist/index.js", // Entry point of your app (adjust if it's not `index.js`)
       instances: "max", // Use max instances based on CPU cores
       exec_mode: "cluster", // Run in cluster mode
       watch: true, // Auto-restart on file changes
